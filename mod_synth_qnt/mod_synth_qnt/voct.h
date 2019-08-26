651b50b3e0097cea350f4ebbd4ab2224e89aca09
Robert Barnes <robertbarnes@googlemail.com>
2019-08-26 09:28:40 +0100

    CHoose lower note if less than higher one so 'zero' is more reliable

--- mod_synth_qnt/mod_synth_qnt/voct.h
+++ mod_synth_qnt/mod_synth_qnt/voct.h
@@ -72,7 +72,7 @@ int quantize_pwm_8_scale(int note, int scale[], int input) {
     }
     // pick note from table scaled to stop overflow
     int index = (i+note) % (PWM_NOTES - 1);
-    if (input < pwm_table_8[index]) {
+    if (input < pwm_table_8[index+1]) {
       return index;
     }
   }
