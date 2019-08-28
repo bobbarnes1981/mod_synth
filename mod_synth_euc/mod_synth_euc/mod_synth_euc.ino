95cc0aa7c04ca2b27aa7b8bc8d8cec4e29ff08c1
Robert Barnes <robertbarnes1981@googlemail.com>
2019-08-28 10:01:40 +0100

    Attempt to remove delay and dejitter analog reads

--- mod_synth_euc/mod_synth_euc/mod_synth_euc.ino
+++ mod_synth_euc/mod_synth_euc/mod_synth_euc.ino
@@ -17,14 +17,22 @@
 #define ANALOG_MAX 1023
 
 #define PULSE_WIDTH 6
+#define IGNORE_ANALOG 2
 
 int current_step = 0;
 int clock_state = LOW;
 
+int prev_raw_steps = 0;
+int prev_raw_pulses = 0;
+int prev_raw_offset = 0;
+
 int selected_steps = -1;
 int selected_pulses = -1;
 int selected_offset = -1;
+
 bool changed = false;
+
+unsigned long triggered = 0;
 
 void setup() {
   pinMode(CLOCK_IN, INPUT);
@@ -43,25 +51,46 @@ void setup() {
 #endif
 }
 
+void setGate(int level) {
+  digitalWrite(GATE_OUT, level);
+  digitalWrite(LED_GATE, level);
+}
+
 void loop() {
+  if (millis() - triggered >= PULSE_WIDTH) {
+    setGate(LOW);
+  }
+  
   // read steps input
-  int requested_steps = map(analogRead(STEPS_IN), ANALOG_MIN, ANALOG_MAX, 2, MAX_STEPS);
-  if (requested_steps != selected_steps) {
-    changed = true;
-    selected_steps = requested_steps;
+  int raw_steps = analogRead(STEPS_IN);
+  if (abs(raw_steps - prev_raw_steps) > IGNORE_ANALOG) {
+    prev_raw_steps = raw_steps;
+    int requested_steps = map(raw_steps, ANALOG_MIN, ANALOG_MAX, 2, MAX_STEPS);
+    if (requested_steps != selected_steps) {
+      changed = true;
+      selected_steps = requested_steps;
+    }
   }
 
   // read pulses input
-  int requested_pulses = map(analogRead(PULSES_IN), ANALOG_MIN, ANALOG_MAX, 1, selected_steps - 1);
-  if (requested_pulses != selected_pulses) {
-    changed = true;
-    selected_pulses = requested_pulses;
+  int raw_pulses = analogRead(PULSES_IN);
+  if (abs(raw_pulses - prev_raw_pulses) > IGNORE_ANALOG) {
+    prev_raw_pulses = raw_pulses;
+    int requested_pulses = map(raw_pulses, ANALOG_MIN, ANALOG_MAX, 1, selected_steps - 1);
+    if (requested_pulses != selected_pulses) {
+      changed = true;
+      selected_pulses = requested_pulses;
+    }
   }
 
   // read offset input
-  int requested_offset = map(analogRead(OFFSET_IN), ANALOG_MIN, ANALOG_MAX, 1, selected_steps - 1);
-  if (requested_offset != selected_offset) {
-    selected_offset = requested_offset;
+  int raw_offset = analogRead(OFFSET_IN);
+  if (abs(raw_offset - prev_raw_offset) > IGNORE_ANALOG) {
+    prev_raw_offset = raw_offset;
+    int requested_offset = map(raw_offset, ANALOG_MIN, ANALOG_MAX, 1, selected_steps - 1);
+    if (requested_offset != selected_offset) {
+      selected_offset = requested_offset;
+    }
   }
 
   if (changed) {
@@ -79,7 +108,6 @@ void loop() {
   }
 
   int clock_in = digitalRead(CLOCK_IN);
-  
   if (clock_in == HIGH && clock_state == LOW) {
     tick(); 
   }
@@ -94,13 +122,8 @@ void tick() {
 #endif
 
   if (output == HIGH) {
-    digitalWrite(GATE_OUT, HIGH);
-    digitalWrite(LED_GATE, HIGH);
-
-    delay(PULSE_WIDTH);
-    
-    digitalWrite(GATE_OUT, LOW);
-    digitalWrite(LED_GATE, LOW);
+    setGate(HIGH);
+    triggered = millis();
   }
   
   current_step++;
